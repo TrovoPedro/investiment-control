bb7bad29f556b5614880f7bb6c761fe77b8777ed
TrovoPedro <pedrotrovo05@outlook.com>
2026-01-17 22:38:19 -0300

    Fix JWT configuration and test properties

--- src/test/kotlin/com/investment/control/investment_control_microservice_person/ApplicationTests.java
+++ src/test/kotlin/com/investment/control/investment_control_microservice_person/ApplicationTests.java
@@ -2,8 +2,10 @@ package com.investment.control.investment_control_microservice_person;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Test
